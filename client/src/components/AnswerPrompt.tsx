bfca35fd05373178f1af028ee1f881c34f1367a0
UmerAalam <umerrazzaq.dev@gmail.com>
2025-08-27 00:59:08 +0500

    css-changes

--- client/src/components/AnswerPrompt.tsx
+++ client/src/components/AnswerPrompt.tsx
@@ -1,6 +1,6 @@
 function AnswerPrompt(props: { answer?: string }) {
   return (
-    <div className="bg-gray-700/20 text-white/90 font-bold flex justify-start w-1/2 items-center border-l-8 border-white/80 px-5 py-3 mt-3 outline-2 hover:outline-gray-700 h-auto min-h-12 rounded-2xl">
+    <div className="bg-gray-700/20 text-white/90 font-bold flex justify-start w-1/2 items-center border-l-8 border-white/80 px-5 py-3 mt-3 h-auto min-h-12 rounded-2xl">
       {props.answer}
     </div>
   );

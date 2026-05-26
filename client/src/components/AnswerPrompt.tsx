1bf597bd4eda67d894706f0f71c367e1c2f85d67
Umer Aalam <umerrazzaq.dev@gmail.com>
2026-05-26 18:27:33 +0500

    more-styles

--- client/src/components/AnswerPrompt.tsx
+++ client/src/components/AnswerPrompt.tsx
@@ -2,7 +2,7 @@ import CopyToClipBoard from "./CopyToClipBoard";
 import MarkdownRenderer from "./MarkdownRenderer";
 function AnswerPrompt(props: { answer?: string }) {
   return (
-    <div className="mr-auto w-full max-w-[75%] bg-gray-700/20 text-white/90 flex justify-start z-0 items-start border-l-8 border-white/80 pl-3 pr-5 py-3 mt-3 h-auto min-h-12 rounded-2xl backdrop-blur-sm">
+    <div className="mr-auto w-full max-w-[75%] bg-gray-700/20 text-white/90 font-semibold flex justify-start z-0 items-start border-l-8 border-white/80 pl-3 pr-5 py-3 mt-3 h-auto min-h-12 rounded-2xl backdrop-blur-sm">
       <div className="min-w-0 w-full leading-relaxed">
         <MarkdownRenderer text={props.answer} />
       </div>
